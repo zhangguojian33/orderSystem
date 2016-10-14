f3fa1fcce00bde36ac5bd11e0a04f4ac0325c46b
gdutzgj@163.com <zhangguojian33>
2016-10-14 17:41:48 +0800

    测试json

--- orderSystem/src/main/java/com/zgj/controller/TestController.java
+++ orderSystem/src/main/java/com/zgj/controller/TestController.java
@@ -1,13 +1,28 @@
 package com.zgj.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller  
 @RequestMapping("/user")  
 public class TestController {
 	 @RequestMapping("/showUser")  
-	    public String toIndex(){ 
+	 public String toIndex(){ 
 		 return "contact";
-	    }  
+	    } 
+	 
+
+	 @RequestMapping("/json")
+	 @ResponseBody
+	 public Object testJson() {
+		 Map<String, String> map =new HashMap<String, String>();  
+		map.put("id", "id");
+		map.put("2", "id");
+		return map;
+	 } 
+
 }
